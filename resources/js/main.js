7a2202e5c3945f2445b788033f14e76d59c77ca0
Sadab Dhrubo <sadab@rocket-pay.io>
2021-11-27 03:49:30 +0600

    responsive done without header

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,67 +1,112 @@
 // code for extra large responsive
 
-let myHtmlElement = document.querySelector("html")
-
-
-
-
-// Responsive html font size
-const iw = window.innerWidth;
-const rootScreenWidth = 1366;
-const rootFontPercentage = 44.46;
-if (iw > 1366) {
-    console.log(iw);
-    let currentFontSize = (rootFontPercentage * iw) / rootScreenWidth;
+function setScreenJsResponsive() {
+  let myHtmlElement = document.querySelector("html")
+  const ow = window.outerWidth;
+  const rootScreenWidth = 1366;
+  const rootFontPercentage = 44.46;
+//   console.log(ow);
+  if (ow >= 1366) {
+    let currentFontSize = (rootFontPercentage * ow) / rootScreenWidth;
     myHtmlElement.setAttribute("style", `font-size:${currentFontSize}%`)
-} else {
+
+  } else {
     myHtmlElement.setAttribute("style", `font-size:${rootFontPercentage}%`)
+  }
 }
 
-window.addEventListener("resize", function () {
-    if (iw > 1366) {
-        console.log(iw);
-        let currentFontSize = (rootFontPercentage * iw) / rootScreenWidth;
-        myHtmlElement.setAttribute("style", `font-size:${currentFontSize}%`)
-    } else {
-        myHtmlElement.setAttribute("style", `font-size:${rootFontPercentage}%`)
-    }
+setScreenJsResponsive()
 
+// Responsive html font size
+
+window.addEventListener("resize", () => {
+    setScreenJsResponsive()
 })
-
-
 
 // Responsive html font size end
-
-
+function el(selector){
+  return document.querySelector(selector)
+}
 
 $('#serviceSliderWrapper').slick({
-    arrow: true,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    prevArrow: $(".serviceSliderPrevBtn"),
-    nextArrow: $(".serviceSliderNextBtn"),
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    speed: 1000,
-})
+  arrow: true,
+  autoplay: false,
+  autoplaySpeed: 5000,
+  prevArrow: $(".serviceSliderPrevBtn"),
+  nextArrow: $(".serviceSliderNextBtn"),
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  speed: 1000,
+  responsive: [
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false,
+        arrow:false,
+        prevArrow: "",
+        nextArrow: "",
+      }
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+
 
 $("#testimonialSlider").slick({
-    arrow: false,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    speed: 500,
-    centerMode: true,
-    centerPadding: '50rem',
-    dots: true,
+  arrow: false,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  speed: 500,
+  centerMode: true,
+  centerPadding: '50rem',
+  dots: true,
+  responsive: [
+    {
+      breakpoint: 769,
+      settings: {
+        centerPadding: '0rem',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false,
+        arrow:false,
+        prevArrow: "",
+        nextArrow: "",
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        centerPadding: '0rem',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false,
+        arrow:false,
+        prevArrow: "",
+        nextArrow: "",
+      }
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+$("#heroSlider").slick({
+  dots: false,
+  infinite: true,
+  speed: 1000,
+  fade: true,
+  cssEase: 'linear',
+  autoplay: true
 })
 
-$("#heroSlider").slick({
-    dots: false,
-    infinite: true,
-    speed: 1000,
-    fade: true,
-    cssEase: 'linear',
-    autoplay: true
-})
+
